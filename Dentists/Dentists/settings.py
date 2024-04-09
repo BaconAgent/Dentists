a0723f3b7655e5e5225ecf7b6397b83d9e4cd183
BaconAgent <stoiandimitrov03@gmail.com>
2024-04-09 11:30:03 +0200

    Attempting to fix access denied issue again

--- Dentists/Dentists/settings.py
+++ Dentists/Dentists/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dentists',
         'USER': 'root',
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'localhost',
         'PORT': '3306',
     }
