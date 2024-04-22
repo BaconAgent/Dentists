3cc91d1aa6a2c8a82fcc69eab27a9887f9879dc4
BaconAgent <stoiandimitrov03@gmail.com>
2024-04-22 15:12:28 +0200

    re-build project due to breaking error

--- DentistsBackend/DentistsBackend/asgi.py
+++ DentistsBackend/DentistsBackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Dentists project.
+ASGI config for DentistsBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dentists.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DentistsBackend.settings')
 
 application = get_asgi_application()
